84a60f95c99197b245c23e4cadd371132b880d7f
BaeHyeJin <bhj305@naver.com>
2025-05-18 19:36:27 +0900

    첨부형 게시판

---
+++ MUSTHAVE계정.sql
@@ -0,0 +1,15 @@
+-- mvc 모델 테이블 생성
+
+DROP TABLE mvcboard;
+create table mvcboard (
+	idx number primary key, 
+	name varchar2(50) not null, 
+	title varchar2(200) not null, 
+	content varchar2(2000) not null, 
+	postdate date default sysdate not null, 
+	ofile varchar2(200), 
+	sfile varchar2(30), 
+	downcount number(5) default 0 not null, 
+	pass varchar2(50) not null, 
+	visitcount number default 0 not null
+);
